abcdea41d94e14e15f7887eef0d95026a1ddbd98
Pawan Jaiswal <pawan.jaiswal09@gmail.com>
2022-03-17 11:43:34 -0500

    Correcting pin diagram and setting the correct voltage to PIR Motion sensor

--- main.py
+++ main.py
@@ -2,17 +2,16 @@ from machine import Pin
 import utime
 
 led = Pin(28, Pin.OUT)
-pir = Pin(16, Pin.IN, Pin.PULL_UP)
+pir = Pin(16, Pin.IN, Pin.PULL_DOWN)  # Default to low voltage
 led.low()
 utime.sleep(3)
 
 while True:
-    # Ideally this should be checking for 0 but the sensor I had didnt give a good voltage
+    # Voltage is high when motion i.e. 1 is detected
     if pir.value() == 1:
         print('Motion Detected!')
         led.high()
         utime.sleep(2)
     else:
-        print('Waiting for movement...')
         led.low()
         utime.sleep(0.2)
